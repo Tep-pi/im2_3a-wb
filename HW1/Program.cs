3da22550716d59e2912b4fd38956461945759fc0
Steffi <steffibauzon@gmail.com>
2025-09-09 19:30:35 +0800

    v1

---
+++ HW1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] word = new string[100];
+            string[] revWord = new string[100];
+            int count = 0;
+
+            while (true)
+            {
+                Console.Write("Enter String: ");
+                word[count] = Console.ReadLine();
+                if (word[count] == "*")
+                {
+                    break;
+                }
+
+                char[] revChar = new char[word[count].Length];
+
+                string toLower = word[count].ToLower();
+
+                for (int i = 0; i <= word[count].Length - 1; i++)
+                {
+                    revChar[i] = word[count][word[count].Length - 1 - i];
+                    revWord[count] += revChar[i];
+                }
+
+                if (revWord[count].ToLower() == word[count].ToLower())
+                {
+                    Console.WriteLine("Original String: " + "\t\t  " + "Reversed: " + "\t\t  " + "Palindrome: ");
+                    Console.WriteLine($"{word[count]} \t\t\t  {revWord[count]} \t\t  True \n");
+                }
+                else
+                {
+                    Console.WriteLine("Original String: " + "\t\t  " + "Reversed: " + "\t\t  " + "Palindrome: ");
+                    Console.WriteLine($"{word[count]} \t\t\t  {revWord[count]} \t\t  False \n");
+                }
+
+                count++;
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HW2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<double>> numbers = new List<List<double>>();
+
+            Console.Write("Enter amount of row: ");
+            int r_num = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter amount of column:");
+            int c_num = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+            double ave = 0;
+
+            for (int r = 0; r < r_num; r++)
+            {
+                List<double> input_num = new List<double>();
+                Console.WriteLine($"\nRows {r + 1}: ");
+
+                for (int c = 0; c < c_num; c++)
+                {
+                    Console.Write($"Enter Number " + (c + 1) + " : ");
+                    double nums = double.Parse(Console.ReadLine());
+                    input_num.Add(nums);
+                    sum += nums;
+                }
+
+                numbers.Add(input_num);
+            }
+
+            Console.Write("\nThe numbers are: \n");
+
+            for (int f = 0; f < numbers.Count; f++)
+            {
+                for (int s = 0; s < numbers[f].Count; s++)
+                {
+                    Console.Write(numbers[f][s] + " ");
+                }
+            }
+
+            Console.WriteLine($"\n\nThe sum is: {sum}");
+
+            ave = sum / (r_num * c_num);
+            Console.WriteLine($"\nThe average is: {ave}");
+
+            Console.ReadKey();
+        }
+    }
+}
