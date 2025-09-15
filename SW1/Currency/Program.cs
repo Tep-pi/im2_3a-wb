27c526dcb255f5e56853d706470bb29908f56707
Steffi <steffibauzon@gmail.com>
2025-09-15 14:55:35 +0800

    v1

---
+++ SW1/Currency/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency
+{
+    class Program
+    {
+        static void converterTiKuwarta(double moneh)
+        {
+            double pesoInD = 57.17;
+            double yenInD = 146.67;
+
+            double P_peso = moneh * pesoInD;
+            double J_yen = moneh * yenInD;
+
+
+            Console.WriteLine(moneh + "\t\t" + P_peso + "\t\t" + J_yen);
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            //Create a method / function to convert  a dollar to peso and yen.
+            //Return multiple values. 1D=146.67yen 1D=57.17peso
+
+            string kuwartaInput;
+            string[] listaTiKuwarta;
+            double kuwarta = 0.0;
+
+            Console.Write("Enter currency in $ (example: 50,100): ");
+            kuwartaInput = Console.ReadLine();
+
+            listaTiKuwarta = kuwartaInput.Split(',');
+
+            Console.WriteLine();
+            Console.WriteLine("Dollars($)" + "\tPeso(P)" + "\t\tYen(Y)");
+
+            for (int m = 0; m<listaTiKuwarta.Length; m++)
+            {
+                string listaKuwarta = listaTiKuwarta[m];
+                kuwarta = double.Parse(listaKuwarta);
+                
+                converterTiKuwarta(kuwarta);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SW1/Prime/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime
+{
+    class Program
+    {
+        static void taga_determine(int numberJayUser)
+        {
+            if (numberJayUser < 2)
+            {
+                Console.WriteLine("Composite");
+                return;
+            }
+
+            for (int i = 2; i < numberJayUser ; i++)
+            {
+                if(numberJayUser % i == 0)
+                {
+                    Console.WriteLine("Composite");
+                    return;
+                }
+            }
+            Console.WriteLine("Prime");
+        }
+        static void Main(string[] args)
+        {
+            //Create a user defined method/function
+            //to determine if it is a prime or not.
+
+            string num_input;
+            string[] numbers_list = new string[50];
+
+            Console.Write("Enter string of numbers: ");
+            num_input = Console.ReadLine();
+
+            numbers_list = num_input.Split(' ');
+
+            for (int n = 0; n < numbers_list.Length; n++)
+            {
+                int user_num = int.Parse(numbers_list[n]);
+                Console.Write($"{n + 1}. {user_num}\t");
+                taga_determine(user_num);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
